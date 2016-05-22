6f67b74d2882a30da5d32437127293c73516b563
Ryu HoSeok <dkflfkdeh@naver.com>
2016-05-22 12:06:34 +0900

    implement item.xml

--- app/src/main/java/com/example/perfectmenu/MainActivity.java
+++ app/src/main/java/com/example/perfectmenu/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.perfectmenu;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.widget.AdapterView.OnItemClickListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,13 +18,15 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     PackageManager myPackageManager;
-
+    Activity act = this;
     public class MyBaseAdapter extends BaseAdapter {
         private Context myContext;
         private List<ResolveInfo> MyAppList;
+        LayoutInflater inflater;
         MyBaseAdapter (Context c, List<ResolveInfo> I){
             myContext = c;
             MyAppList = I;
+            inflater = (LayoutInflater) act.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
         @Override
         public int getCount(){
@@ -37,11 +42,37 @@ public class MainActivity extends AppCompatActivity {
         }
         @Override
         public View getView(int position, View convertView, ViewGroup parent){
-            ImageView imageView;
+            if (convertView == null){
+                convertView = inflater.inflate(R.layout.item, parent, false);
+            }
+            final ResolveInfo info = MyAppList.get(position);
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8,8,8,8);
+            TextView textView = (TextView) convertView.findViewById(R.id.textView);
+            imageView.setImageDrawable(info.loadIcon(myPackageManager));
+            textView.setText(info.loadLabel(myPackageManager).toString());
+            Log.v("[Program]", info.activityInfo.packageName+","+info.activityInfo.name);
+
+            imageView.setOnClickListener(new Button.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_RUN);
+                    intent.setComponent(new ComponentName(
+                            info.activityInfo.packageName,
+                            info.activityInfo.name));
+                    act.startActivity(intent);
+                    String msg = info.activityInfo.packageName+", "+info.activityInfo.name;
+                    Toast.makeText(act,msg,Toast.LENGTH_LONG).show();
+                }
+            });
+
+            return convertView;
+            /*ImageView imageView;
             if(convertView == null){
                 //not recycled -> initialize
                 imageView = new ImageView(myContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setLayoutParams(new GridView.LayoutParams(80, 80));
                 imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 imageView.setPadding(8,8,8,8);
             }
@@ -50,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             }
             ResolveInfo resolveInfo = MyAppList.get(position);
             imageView.setImageDrawable(resolveInfo.loadIcon(myPackageManager));
-            return imageView;
+            return imageView;*/
 
         }
     }
